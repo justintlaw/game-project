6c635f28bcbc3951027c7c6d65d5133cb1ccb380
Justin Lawrence <justin.lawrence@byu.edu>
2021-09-07 14:55:45 -0600

    fix table permissions

--- lib/game-project-stack.ts
+++ lib/game-project-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core'
 import * as apigateway from '@aws-cdk/aws-apigateway'
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as dynamodb from '@aws-cdk/aws-dynamodb'
+import * as path from 'path'
 import { Duration } from '@aws-cdk/core'
 
 export class GameProjectStack extends cdk.Stack {
@@ -16,13 +17,16 @@ export class GameProjectStack extends cdk.Stack {
     // Create the lambda infrastructure for the endpoints
     const endpointsLambda = new lambda.Function(this, 'EndpointsLambda', {
       runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset('../src'),
+      code: lambda.Code.fromAsset(path.join(__dirname, '../src')),
       handler: 'handler/index.handler',
       environment: {
         TABLE_NAME: gameTable.tableName
       },
       timeout: Duration.seconds(6)
     })
+
+    // give the lambda access to the database
+    gameTable.grantReadWriteData(endpointsLambda)
 
     // Create the API Gateway Lambda integration
     new apigateway.LambdaRestApi(this, 'GameAPI', {
